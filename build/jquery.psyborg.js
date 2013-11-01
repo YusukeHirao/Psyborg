a743442bd146b0c4b0ce57e0b7e1ff6f73c13c83
YusukeHirao <yusukehirao@me.com>
2013-11-01 21:17:26 +0900

    dev (grunt commit r740)

--- build/jquery.psyborg.js
+++ build/jquery.psyborg.js
@@ -1,5 +1,5 @@
 /**
- * Psyborg.js - v0.3.0 r739
+ * Psyborg.js - v0.3.0 r740
  * update: 2013-11-01
  * Author: Yusuke Hirao [http://www.yusukehirao.com]
  * Github: https://github.com/YusukeHirao/Psyborg
