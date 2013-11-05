1583fd74eb782793c688cf26beafec9d8af0b395
YusukeHirao <yusukehirao@me.com>
2013-11-05 10:15:59 +0900

    dev (grunt commit r747)

--- build/jquery.psyborg.js
+++ build/jquery.psyborg.js
@@ -1,5 +1,5 @@
 /**
- * Psyborg.js - v0.3.0 r746
+ * Psyborg.js - v0.3.0 r747
  * update: 2013-11-05
  * Author: Yusuke Hirao [http://www.yusukehirao.com]
  * Github: https://github.com/YusukeHirao/Psyborg
@@ -783,50 +783,51 @@ PsycleTransition.create({
         }
     }
 });
-PsycleTransition.create({
-    fade: {
-        init: function () {
-            // スタイルを設定
-            PsyborgCSS.posBase(this.stage.$el);
-            PsyborgCSS.posAbs(this.container.$el);
-            PsyborgCSS.posAbs(this.panels.$el);
-            // 初期化時のインラインスタイルを保持
-            // var $panel:JQuery = this.panels.$el;
-            // $panel.data('originStyle', $panel.attr('style'));
-        },
-        reflow: function (info) {
-            switch (info.timing) {
-                case PsycleReflowTiming.TRANSITION_END:
-                case PsycleReflowTiming.RESIZE_START:
-                case PsycleReflowTiming.RESIZE_END:
-                    console.log(this.panels);
-                    PsyborgCSS.z(this.panels.$el, 0);
-                    PsyborgCSS.z(this.panels.item(this.to).$el, 10);
-                    break;
-            }
-        },
-        silent: function () {
-        },
-        before: function () {
-            this.panels.item(this.to).$el.css({ opacity: 0 });
-        },
-        fire: function () {
-            if (this.animation) {
-                this.animation.stop();
-            }
-            this.animation = $.Animation(this.panels.item(this.to).$el[0], {
-                opacity: 1
-            }, {
-                duration: this._config.duration
-            });
-        },
-        cancel: function () {
-        },
-        after: function () {
-        }
-    }
-});
 
+// PsycleTransition.create({
+// 	fade: {
+// 		init: function ():void {
+// 			// スタイルを設定
+// 			PsyborgCSS.posBase(this.stage.$el);
+// 			PsyborgCSS.posAbs(this.container.$el);
+// 			PsyborgCSS.posAbs(this.panels.$el);
+// 			// 初期化時のインラインスタイルを保持
+// 			// var $panel:JQuery = this.panels.$el;
+// 			// $panel.data('originStyle', $panel.attr('style'));
+// 		},
+// 		reflow: function (info:IPsycleReflowInfo):void {
+// 			switch (info.timing) {
+// 				case PsycleReflowTiming.TRANSITION_END:
+// 				case PsycleReflowTiming.RESIZE_START:
+// 				case PsycleReflowTiming.RESIZE_END:
+// 					console.log(this.panels);
+// 					PsyborgCSS.z(this.panels.$el, 0);
+// 					PsyborgCSS.z(this.panels.item(this.to).$el, 10);
+// 					break;
+// 			}
+// 		},
+// 		silent: function ():void {},
+// 		before: function ():void {
+// 			this.panels.item(this.to).$el.css({ opacity:<number> 0 });
+// 		},
+// 		fire: function ():any {
+// 			if (this.animation) {
+// 				this.animation.stop();
+// 			}
+// 			this.animation = $.Animation(
+// 				this.panels.item(this.to).$el[0],
+// 				{
+// 					opacity:<number> 1
+// 				},
+// 				{
+// 					duration:<number> this._config.duration
+// 				}
+// 			);
+// 		},
+// 		cancel: function ():void {},
+// 		after: function ():void {}
+// 	}
+// });
 /**!
 * スライド要素を生成・管理するクラス
 *
